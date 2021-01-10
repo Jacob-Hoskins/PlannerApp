86171a56fc57796e0d078b8695b50b8c19f2ccaf
Jacob Hoskins <45977627+Jacob-Hoskins@users.noreply.github.com>
2021-01-10 16:02:11 -0500

    Add files via upload
    Restructed the code so it is easier to read and understand. updated homescreen to get rid of adding task option and gave that its own page and when youre on other pages the sidebar is gone and one button will send you to the home page to get to other fucntions

--- main.py
+++ main.py
@@ -34,6 +34,13 @@ class showTask():
 class PunchClock():
 
     def __init__(self, master):
+# Home Button
+        self.side = LabelFrame(master)
+        self.side.grid(row=0, column=0)
+
+        self.homeButton = Button(self.side, text="-\n-\n-", command=self.home, padx=20, pady=-10)
+        self.homeButton.grid(row=0, column=0)
+
         self.clock = Label(master, text="TIME", font=("Helvetica", 48), fg="green", bg="black")
         self.clock.grid(row=0, column=1, padx=50)
         self.clock.after(500, self.clockEngine)
@@ -50,17 +57,20 @@ class PunchClock():
         pass
 
 
-#todo create columns in database for task completeion date
-#todo fix date dropdown menus, make dates match month date
-class MainScreen():
+    def home(self):
+        self.master.destroy()
+        self.side.destroy()
+        MainScreen(root)
 
+class ToDo():
     def __init__(self, master):
 
         #Frames
         self.taskFrame = LabelFrame(master)
         self.taskFrame.grid(row=0, column=1, pady=5)
-        self.sideBar = LabelFrame(master, padx=10, pady=16)
-        self.sideBar.grid(row=0, column=0, padx=10, pady=5)
+#Home Button
+        self.side = LabelFrame(master)
+        self.side.grid(row=0, column=0)
 
         #Labels
         self.taskLabel = Label(self.taskFrame, text="Task")
@@ -73,20 +83,12 @@ class MainScreen():
         self.taskEntry.grid(row=1, column=2)
 
         #buttons
-        self.homeButton = Button(self.sideBar, text="Home", command=self.homeFunc)
-        self.homeButton.grid(row=0, column=0)
-        self.punchclockButton = Button(self.sideBar, text="PunchClock", command=self.punchclockClicked)
-        self.punchclockButton.grid(row=3, column=0)
-        self.settingsButton = Button(self.sideBar, text="Settings", padx=12, command=self.settingsButton)
-        self.settingsButton.grid(row=4, column=0)
         self.confirmButton = Button(self.taskFrame, text="Add Task", command=self.addTask)
         self.confirmButton.grid(row=5, column=2, pady=10)
-        self.shareButton = Button(self.sideBar, text="Share Task", padx=5)
-        self.shareButton.grid(row=2, column=0)
-        self.viewTask = Button(self.sideBar, text="View Task", padx=7, command=self.viewTask)
-        self.viewTask.grid(row=1, column=0)
+        self.homeButton = Button(self.side, text="-\n-\n-", command=self.home, padx=20, pady=-10)
+        self.homeButton.grid(row=0, column=0)
 
-        #combobox
+        # combobox
         self.monthVar = StringVar()
         self.monthVar.set(month[0])
         self.monthDrop = OptionMenu(self.taskFrame, self.monthVar, *month)
@@ -115,9 +117,8 @@ class MainScreen():
         self.minuteDrop = OptionMenu(self.taskFrame, self.minuteVar, *self.minute)
         self.minuteDrop.grid(row=3, column=4)
 
-
     def addTask(self):
-        #gets task info
+        # gets task info
         self.task = self.taskEntry.get()
         self.dateStart = datetime.now()
         self.deadlineMonth = self.monthVar.get()
@@ -126,7 +127,7 @@ class MainScreen():
         self.deadlineHour = self.hourVar.get()
         self.deadlineMinute = self.minuteVar.get()
 
-        #prints task info
+        # prints task info
         print(self.task)
         print(self.dateStart)
         print(self.deadlineMonth)
@@ -135,26 +136,63 @@ class MainScreen():
         print(self.deadlineHour)
         print(self.deadlineMinute)
 
-        #add task to database
-        mycursor.execute("INSERT INTO task(TaskName, created) VALUES(%s,%s)", (self.task, self.dateStart))
+        # add task to database
+        mycursor.execute("INSERT INTO task(TaskName, created, monthDeadline, dayDeadline,"
+                         " weekDayDeadline, hourDeadline, minuteDeadline,"
+                         "amPm) VALUES(%s,%s,%s,%s,%s,%s,%s)",
+                         (self.task, self.dateStart, self.deadlineMonth, self.deadlineMonthDay,
+                          self.deadlineDay, self.deadlineHour, self.deadlineMinute))
         db.commit()
 
-        #clear task entry
+        # clear task entry
         self.taskEntry.delete(0, END)
+
+    def home(self):
+        self.taskFrame.destroy()
+        self.side.destroy()
+        MainScreen(root)
+
+
+#todo create columns in database for task completeion date
+#todo fix date dropdown menus, make dates match month date
+class MainScreen():
+
+    def __init__(self, master):
+
+        #Frames
+        self.taskFrame = LabelFrame(master)
+        self.taskFrame.grid(row=0, column=1, pady=5)
+        self.sideBar = LabelFrame(master, padx=10, pady=16)
+        self.sideBar.grid(row=0, column=0, padx=10, pady=5)
+
+        #buttons
+        self.toDoButton = Button(self.sideBar, text="Add Task", command=self.taskFunc)
+        self.toDoButton.grid(row=1, column=0)
+        self.punchclockButton = Button(self.sideBar, text="PunchClock", command=self.punchclockClicked)
+        self.punchclockButton.grid(row=4, column=0)
+        self.settingsButton = Button(self.sideBar, text="Settings", padx=12, command=self.settingsButton)
+        self.settingsButton.grid(row=5, column=0)
+        self.shareButton = Button(self.sideBar, text="Share Task", padx=5)
+        self.shareButton.grid(row=3, column=0)
+        self.viewTask = Button(self.sideBar, text="View Task", padx=7, command=self.viewTask)
+        self.viewTask.grid(row=2, column=0)
+
+    def taskFunc(self):
+        self.taskFrame.destroy()
+        self.sideBar.destroy()
+        ToDo(root)
 
     def punchclockClicked(self):
         self.taskFrame.destroy()
+        self.sideBar.destroy()
         PunchClock(root)
 
     def settingsButton(self):
         print("Clicked")
 
-    def homeFunc(self):
-        #fixme: if home button clicked more than once you cant change screens
-        MainScreen(root)
-
     def viewTask(self):
         self.taskFrame.destroy()
+        self.sideBar.destroy()
         showTask(root)
 
 
